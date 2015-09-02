2c992fceadff401e20d0faff9eb21c89e220457f
Jignesh <patiljignesh@gmail.com>
2015-09-01 23:18:43 -0700

    Added a Variable to the print statement.

---
+++ PrintingSomething/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingSomething
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int someNumber = 10;
+            Console.WriteLine("This is how to print a variable {0}",someNumber);
+            Console.WriteLine("I can write this too!");
+            Console.ReadKey();
+        }
+    }
+}
